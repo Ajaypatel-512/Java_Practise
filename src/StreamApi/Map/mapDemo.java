ec1cb943fe2c7e6d2d6f1af4f4df3523744df38e
Ajaypatel <Ajaypatelap1504@gmail.com>
2022-10-18 11:29:56 +0530

    added more program

--- src/StreamApi/Map/mapDemo.java
+++ src/StreamApi/Map/mapDemo.java
@@ -1,6 +1,9 @@
 package StreamApi.Map;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class mapDemo {
@@ -13,10 +16,19 @@ public class mapDemo {
         arr.add(22);
         arr.add(98);
         arr.add(1);
-
         System.out.println(arr);
 
         ans = (ArrayList<Integer>) arr.stream().map(n->n/10).collect(Collectors.toList());
         System.out.println(ans);
+
+
+        List<String> vehicle = Arrays.asList("bus","train", "cycle","bike","ship");
+        List<String> result = new ArrayList<>();
+
+        result = vehicle.stream().map(i->i.toUpperCase()).collect(Collectors.toList());
+        System.out.println(result);
+
+        vehicle.stream().map(i->i.length()).forEach(len-> System.out.print(len+" "));
+
     }
 }
