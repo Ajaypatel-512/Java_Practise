d71e6edcaf50a008b6c693ebdc89c04e588b0ab5
Ajaypatel <Ajaypatelap1504@gmail.com>
2022-10-23 21:12:00 +0530

    adding Thread package ABC class

---
+++ src/Thread/ABC.java
@@ -0,0 +1,25 @@
+package Thread;
+
+public class ABC implements Runnable{
+
+    @Override
+    public void run() {
+        try {
+            // moving thread t2 to the state timed waiting
+            Thread.sleep(100);
+        }
+        catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
+        System.out.println("The state of thread t1 while it invoked the method join() on thread t2 -"+ demo.t1.getState());
+
+        // try-catch block
+        try {
+            Thread.sleep(200);
+        }
+        catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
+}
