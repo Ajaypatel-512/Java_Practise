57acbe9aa60f58c2a13ebe4b7992ea0d125b9df4
Ajaypatel <Ajaypatelap1504@gmail.com>
2023-11-26 18:30:08 +0530

    Java 9 Features: Private static Interface Methods

--- src/Java_9_Features/private_interface/SendNotifications.java
+++ src/Java_9_Features/private_interface/SendNotifications.java
@@ -2,20 +2,26 @@ package Java_9_Features.private_interface;
 
 public interface SendNotifications {
 
-    default void sendNotifications(){
+     static void sendNotifications(){
 //        System.out.println("Establishing a connection");
-        establishConnection();
+//        establishConnection();
+        retryingConnection();
         System.out.println("Sending multiple Notifications");
     }
 
     default void sendNotification(){
 //        System.out.println("Establishing a connection");
         establishConnection();
+        retryingConnection();
         System.out.println("Sending a Notifications");
     }
 
     private void establishConnection(){
         System.out.println("Establishing a connection");
+    }
+
+    private static void retryingConnection(){
+        System.out.println("Retrying to Establish a connection");
     }
 
 }
--- src/Java_9_Features/private_interface/SendNotificationsImpl.java
+++ src/Java_9_Features/private_interface/SendNotificationsImpl.java
@@ -5,7 +5,8 @@ public class SendNotificationsImpl implements SendNotifications{
     public static void main(String[] args) {
         SendNotifications sendNotifications = new SendNotificationsImpl();
         sendNotifications.sendNotification();
-        sendNotifications.sendNotifications();
+//        sendNotifications.sendNotifications(); static method not called by object name
+        SendNotifications.sendNotifications(); //static method called by Class/Interface name
     }
 
 }
