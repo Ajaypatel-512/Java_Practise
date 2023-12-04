8d549259a46f3069762e4887e626bb997d4a9979
Ajaypatel <Ajaypatelap1504@gmail.com>
2023-12-04 10:09:32 +0530

    Java 13 and 14 Features: NullPointer

---
+++ src/Java_13_14_Features/NullPointer/NullPointer.java
@@ -0,0 +1,8 @@
+package Java_13_14_Features.NullPointer;
+
+public class NullPointer {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.charAt(1));
+    }
+}
