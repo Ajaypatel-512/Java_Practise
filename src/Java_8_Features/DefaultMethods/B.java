8ad939a1b92a36b00e13cd349840ac537985f3ff
Ajaypatel <Ajaypatelap1504@gmail.com>
2023-12-08 18:00:13 +0530

    Java 8 Features: Deafult methods

---
+++ src/Java_8_Features/DefaultMethods/B.java
@@ -0,0 +1,7 @@
+package Java_8_Features.DefaultMethods;
+
+public class B implements A{
+    public void m1(){
+        System.out.println("M1 Inside B");
+    }
+}
---
+++ src/Java_8_Features/DefaultMethods/Test.java
@@ -0,0 +1,8 @@
+package Java_8_Features.DefaultMethods;
+
+public class Test {
+    public static void main(String[] args) {
+        A a = new B();
+        a.m1();
+    }
+}
