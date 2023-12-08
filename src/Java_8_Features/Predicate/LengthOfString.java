0b0bccda17032a0e3da17138589f8a15eaa4a97e
Ajaypatel <Ajaypatelap1504@gmail.com>
2023-12-08 18:10:51 +0530

    Java 8 Features: Predicates

---
+++ src/Java_8_Features/Predicate/LengthOfString.java
@@ -0,0 +1,12 @@
+package Java_8_Features.Predicate;
+
+import java.util.function.Predicate;
+
+public class LengthOfString {
+    public static void main(String[] args) {
+        Predicate<String> s = s1 -> (s1.length()>5);
+        System.out.println(s.test("abc"));
+        System.out.println(s.test("abcweffv"));
+
+    }
+}
