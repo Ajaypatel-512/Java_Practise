77b531eb19fb4667ba0d3ce1e42c97d68b6c77c7
Ajaypatel <Ajaypatelap1504@gmail.com>
2023-12-13 21:06:24 +0530

    Java Collections Framework: Random

---
+++ src/Collections_Framework/Set/SetDemo.java
@@ -0,0 +1,18 @@
+package Collections_Framework.Set;
+
+public class SetDemo {
+    public static void main(String[] args) {
+        /*
+        * HashSet stores the elements by using a mechanism called hashing.
+        * HashSet contains unique elements only.
+        * HashSet allows null value.
+        * HashSet class is non synchronized.
+        * HashSet doesn't maintain the insertion order. Here, elements are inserted on the basis of their hashcode.
+        * HashSet is the best approach for search operations.
+        * The initial default capacity of HashSet is 16, and the load factor is 0.75.
+        * */
+
+
+
+    }
+}
