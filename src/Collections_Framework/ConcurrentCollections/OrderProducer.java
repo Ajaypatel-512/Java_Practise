e531ac199f6a658922970fbb28d5c04a5d09e74b
Ajaypatel <Ajaypatelap1504@gmail.com>
2023-12-16 20:40:33 +0530

    Java Collections Framework: concurrent collections

---
+++ src/Collections_Framework/ConcurrentCollections/OrderProducer.java
@@ -0,0 +1,26 @@
+package Collections_Framework.ConcurrentCollections;
+
+import java.util.concurrent.BlockingQueue;
+
+public class OrderProducer implements Runnable {
+
+    private BlockingQueue<String> queue;
+
+    OrderProducer(BlockingQueue<String> queue){
+        this.queue = queue;
+    }
+
+
+    @Override
+    public void run() {
+
+        try {
+            queue.put("Mac Laptop");
+            queue.put("Dell");
+            queue.put("Hp");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
